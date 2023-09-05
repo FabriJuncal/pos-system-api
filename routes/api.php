c4c5b2e70e693ca9481bdb3cdfe4b13cd267369f
FabriJuncal <juncalfabri@gmail.com>
2023-09-04 22:35:11 -0300

    feat(auth): Se implementó endpoint de registro y autenticación de usuario

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+// Se definen las rutas que no estarán protegidas por Sanctum, ya que no son requeridas.
+// En esta instancia recien se obtendrá el API TOKEN para poder acceder a las rutas protegidas por Sanctum.
+Route::post('auth/register',[AuthController::class, 'create']);
+Route::post('auth/login',[AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
