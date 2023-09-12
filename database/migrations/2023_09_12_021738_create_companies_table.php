7ab263842c571facb603be3f3afacab62e57e321
FabriJuncal <juncalfabri@gmail.com>
2023-09-12 00:23:45 -0300

    feat(migrations): se crearon las migraciones para las empresas y locales

---
+++ database/migrations/2023_09_12_021738_create_companies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('colors', 255)->nullable();
+            $table->string('logo', 255)->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('company_categories_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
